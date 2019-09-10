31a2020107a9e99b1a41fc920df34bc5c8adc1ee
Alexandre Senges <alexandre.senges@ermeo.com>
2019-09-10 03:20:02 +0200

    Moved images to use github

--- serve_images/serve_images.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = new express();
-
-const port = 3010;
-
-app.get('/images/:img', (req, res) => res.sendFile(`${__dirname}/images/${req.params.img}`));
-
-app.listen(port, () => console.log(`Running on port ${port}!`));
