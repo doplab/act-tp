93213479c9940a2776451a8205f83e843cb28522
admin <admin@wl2s-166-56.unil.ch>
2020-10-01 17:07:51 +0200

    TP4 (almost) ready

--- 2020/week4/resources/question13.py
+++ 2020/week4/resources/question13.py
@@ -1,4 +1,4 @@
 liste_finale = []
 tuple_initial = (1, 4, 5, 8)
-for valeur in tuple_initial:          # A chaque iteration valeur est une copie d'un element du tuple. La boucle se finie au dernier element du tuple_initial.
-	liste_finale.append(valeur)   # On ajoute valeur a la liste cree en dehors de la boucle.
+for valeur in tuple_initial:          # A chaque iteration valeur devient une copie d'un element du tuple. La boucle se termine au dernier élément de tuple_initial.
+	liste_finale.append(valeur)   # On ajoute valeur à la liste créée en dehors de la boucle.
