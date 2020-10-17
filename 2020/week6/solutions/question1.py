4e67b10676dddf2797e95c354fc7d352d0062ee0
Gaëtan Michelet <55562103+guymono@users.noreply.github.com>
2020-10-17 22:46:48 +0200

    modification

--- 2020/week6/solutions/question1.py
+++ 2020/week6/solutions/question1.py
@@ -1,19 +1,19 @@
     #Définition de la fonction
     def recherche_sequentielle(liste, x):
-        for i in range(len(liste)): #i représente l'index 
-            if liste[i] == x:
-                print("X est présent dans la liste à l'index :", i)
-                return i
-                
-            else: #Utilisez "else" s'il n'y a pas de "break"
-                print("X n'est pas présent dans la liste")
-                return -1
-    
-    #Déclaration de la liste et de la variable x 
-    L = [3,55,6,8,3,5,56,33,6,5,3,2,99,53,532,75,21,963,100,445,56,45,12,56,24]
-    x = 100
-    
-    #Exécution de l'algorithme
-    recherche_sequentielle(L, x)
+    for i in range(len(liste)):  # i représente l'index
+        if liste[i] == x:
+            print("X est présent dans la liste à l'index :", i)
+            return i
+
+
+    print("X n'est pas présent dans la liste")
+    return -1
+
+# Déclaration de la liste et de la variable x
+L = [3, 55, 6, 8, 3, 5, 56, 33, 6, 5, 3, 2, 99, 53, 532, 75, 21, 963, 100, 445, 56, 45, 12, 56, 24]
+x = 100
+
+# Exécution de l'algorithme
+recherche_sequentielle(L, x)
   
   
