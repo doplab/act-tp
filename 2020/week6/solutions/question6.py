13fb3392e406467aa4d6c583f4e1b97dfc467369
Gaëtan Michelet <55562103+guymono@users.noreply.github.com>
2020-10-17 22:52:50 +0200

    -1 au lieu de False / len au lieu de len-1

--- 2020/week6/solutions/question6.py
+++ 2020/week6/solutions/question6.py
@@ -1,6 +1,6 @@
 def recherche_binaire(liste,e):
     first = 0 #correspond à l'index du premier élément de la liste
-    last = len(liste)-1 #correspond à l'index du dernier élément de la liste
+    last = len(liste) #correspond à l'index du dernier élément de la liste
     #SOLUTION
     while first <= last:
         mid = int((first+last)/2) #l'élément du milieu. La fonction int() permet d'obtenir un entier dans le cas ou "first + last" est un nombre impair
@@ -12,7 +12,7 @@ def recherche_binaire(liste,e):
                 last = mid-1
             else:
                 first = mid+1
-    return False # Si la condition de la ligne 8 n'est jamais remplie, la fonction retourne "False" 
+    return -1 # Si la condition de la ligne 8 n'est jamais remplie, la fonction retourne -1 
 
 L = [1231321,3213125,3284016,4729273,5492710]
 e = 3284016
