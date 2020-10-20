9728af6087f11fbcd763488097449118227f0b17
Gaëtan Michelet <55562103+guymono@users.noreply.github.com>
2020-10-20 17:00:34 +0200

    correction

--- 2020/week6/solutions/question4-1.py
+++ 2020/week6/solutions/question4-1.py
@@ -11,6 +11,10 @@ def recherche_binaire_recursive(L, s, r, x):
     else:  
         return -1
     
-    
+L = [1,3,4,5,7,8,9,15]
+s = 0
+r = len(L)-1
+x = 7
+print(recherche_binaire_iterative(L,s, r, x))
    
     
