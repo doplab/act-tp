a60049a0b6ae71b6629b27d749205b16da1aee81
Gaëtan Michelet <55562103+guymono@users.noreply.github.com>
2020-10-22 16:39:51 +0200

    ajout code exercices

---
+++ 2020/consolidation1/exercices/binary_search_closest_exercice.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.util.List;
+
+public class Main {
+
+    public static int recherche_binaire_recursive(List L,int s,int r,int e){  
+        //TODO
+    }
+
+    public static int plus_proche(List L,int e,int v){  
+	//TODO
+    }
+
+    public static void main(String[] args) {
+        List L = List.of(1,2,5,8,12,16,24,56,58,63);
+        int s = 0;
+        int r = L.size()-1;
+        int e = 64;
+        System.out.println(plus_proche(L,e,recherche_binaire_recursive(L,s,r,e)));
+        }
+    }
