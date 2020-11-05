97ed927010bb2429dea695b9e4e2f14f507b31d7
jschwab55 <70637615+jschwab55@users.noreply.github.com>
2020-11-05 15:36:11 +0100

    Update Question9_solution.py

--- 2020/Week 7/Question9_solution.py
+++ 2020/Week 7/Question9_solution.py
@@ -7,20 +7,23 @@ def dijkstra(origin,destination,visited = None):
     if visited is None:#A l'initialisation, l'ensemble des sommets atteints est vide
         visited = set()
     
+    #Question 9.1
     if origin.value == destination: #Si on est arrivé à destination, terminer l'algorithme => return
         return(0, origin.value)
     
     distance = inf
     path = origin.value
-    visited.add(origin.value)#Ajoute le point à l'ensemble des sommets visités
+    visited.add(origin.value)#Ajoute le point à l'enseble des sommets visités
     
+    #Question 9.2
     for relationship in origin.relationships:
         
+        #Question 9.3
         if relationship.value == 99999:
             continue
         
         neighbour = relationship.to #Les voisins atteignables
-        
+
         
         if neighbour.value not in visited:#Si un des voisins n'a pas encore été visités
             distance_temp, path_temp = dijkstra(neighbour, destination, visited) #On se déplace sur ce point et fait l'algo à partir de ce points.
@@ -29,7 +32,7 @@ def dijkstra(origin,destination,visited = None):
             total_distance = distance_temp + relationship.value #Distance du chemin optimal à partir du neighbor + distance entre le point de départ et le neighbour.
                                                                   
                                                                 
-                                                                
+            #Question 9.4                                                     
             if total_distance < distance: #Si le chemin en question est meilleur que les précédents, on le sauvegarde.
                 distance = total_distance
                 path = origin.value + path_temp 
