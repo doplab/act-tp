144e4dc22d329132271583bccf530d01194938cf
jschwab55 <70637615+jschwab55@users.noreply.github.com>
2020-11-09 20:31:14 +0100

    Update Question2_solution.py

--- 2020/Week 8/Question2_solution.py
+++ 2020/Week 8/Question2_solution.py
@@ -1,4 +1,7 @@
 #Question 2
+#Librairie qui permettent de visualiser le résultat (pas nécessaire à la résolution du problème)
+import matplotlib.pyplot as plt
+import numpy as np
 
 def nearest_neighbor(start, point_set):#start correspond au point de départ, point_set correspond à l'ensemble des points
     for i in range(len(point_set)):#on parcourt tout les point de l'ensemble
@@ -18,3 +21,17 @@ def nearest_neighbor(start, point_set):#start correspond au point de départ, po
         #parcouru jusqu'ici. Si c'est le cas, on redéfinit la distance minimale et "enregistre" les coordonées du point
         
     return nearest_nei, min_distance
+
+a = np.array([(1,2), (5,6), (7,8), (2,5), (9,1)])#List de points
+b = (3,4)#Point de départ
+
+point, distance = nearest_neighbor(b,a)
+#Devrait output [2,5]
+
+print("Le voisin le plus proche est ", point, " avec une distance de ", distance)
+
+#Vous ne devez pas comprendre ce bout de code, il vous permet de visualiser le résultat
+#Le point de départ apparait en orange, le voisin le plus proche en rouge
+plt.scatter(a[:,0],a[:,1])
+plt.scatter(b[0], b[1], color = 'orange')
+plt.scatter(point[0], point[1], color = 'green')
