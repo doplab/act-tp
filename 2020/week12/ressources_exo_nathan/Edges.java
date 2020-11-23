40804b7c07e2034b9a890a73e83da271a5cf9519
Nathan Ballèvre <63353024+pastouche2@users.noreply.github.com>
2020-11-23 08:31:22 +0100

    Add files via upload

---
+++ 2020/week12/ressources_exo_nathan/Edges.java
@@ -0,0 +1,24 @@
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Edges {
+    public String from_vertex; // node de départ
+    public String to_vertex;  // node d'arrivée ( chaîne de caractère)
+    public double weight;  // poids de l'arête
+
+    public Edges(String from_vertex, String to_vertex, double weight) {
+        this.from_vertex = from_vertex;
+        this.to_vertex = to_vertex;
+        this.weight = weight;
+    }
+
+    public void print(){
+        Map<String, String> edge_rep = new HashMap <String,String>(); // Création d'un dictionnaire pour pouvoir afficher une arête
+        edge_rep.put("from_vertex",this.from_vertex);
+        edge_rep.put("to_vertex",this.to_vertex);
+        edge_rep.put("weight", String.valueOf(this.weight));
+        System.out.println(edge_rep);
+    }
+
+}
