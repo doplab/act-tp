43a01930f04bdd70302672e1369527237bfc1d83
Alpha Diallo <alpha@Alphas-MacBook-Pro.local>
2020-11-24 15:12:28 +0100

    Piece.py

--- 2020/week10/Piece.py
+++ 2020/week10/Piece.py
@@ -1,8 +1,8 @@
-import numpy as np
+import random
 
 #La fonction Piece retourn une liste contenant des 0 et des 1, considérez un 1 comme un succès, i.e. une fois ou la pièce tombe sur pile, et 0 comme un échec
 def Piece(l):
-    return np.random.randint(0,2,l)
+    return [random.randint(0, 1) for i in range(l)]
 
 
 
