23e57131d47891a27aaa04b09b562244f37b6ce1
Gaëtan Michelet <55562103+guymono@users.noreply.github.com>
2020-11-28 16:26:11 +0100

    Update main2solution.java

--- 2020/week12/ressources/main2solution.java
+++ 2020/week12/ressources/main2solution.java
@@ -1,12 +1,14 @@
-P1 a encore 50 points de vie
-P2 a encore 30 points de vie
-P3 a encore 50 points de vie
-P1 a encore 50 points de vie
-P2 a encore 10 points de vie
-P3 a encore 50 points de vie
+P1 a encore 10 points de vie
+P2 a encore 8 points de vie
+P3 a encore 10 points de vie
+P1 a encore 10 points de vie
+P2 a encore 8 points de vie
+P3 a encore 10 points de vie
+P1 a encore 10 points de vie
+P2 a encore 4 points de vie
+P3 a encore 10 points de vie
 P2 est mort
-P1 a encore 50 points de vie
-P3 a encore 50 points de vie
-P2 est déjà mort
+P1 a encore 10 points de vie
+P3 a encore 10 points de vie
 
 Process finished with exit code 0
