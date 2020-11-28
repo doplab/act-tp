a26a5eff2f26cdb97952fa4e5c74e2248ec12c59
Gaëtan Michelet <55562103+guymono@users.noreply.github.com>
2020-11-28 16:25:30 +0100

    Update main2.java

--- 2020/week12/ressources/main2.java
+++ 2020/week12/ressources/main2.java
@@ -1,12 +1,11 @@
 public class Main {
-
     public static void main(String[] args) {
-        Fighter P1 = new Fighter("P1", 50, 30, 10);
-        Fighter P2 = new Fighter("P2", 50, 25, 10);
-        Fighter P3 =  new Fighter("P3", 50, 25, 10);
-        P1.attack(P2);
-        P1.attack(P2);
-        P1.attack(P2);
-        P1.attack(P2);
+        Combattant P1 = new Combattant("P1", 10, 2, 2);
+        Combattant P2 = new Combattant("P2", 10, 2, 2);
+        Combattant P3 =  new Combattant("P3", 10, 2, 2);
+        P1.attack("pied",P2);
+        P1.attack("poing",P2);
+        P1.attack("tete",P2);
+        P1.attack("tete",P2);
     }
 }
