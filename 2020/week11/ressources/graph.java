8d434a004c5c37795748cb550f072b6bbef7d734
Alpha Diallo <alpha@Alphas-MacBook-Pro.local>
2020-11-30 00:22:03 +0100

    POO in Python

--- 2020/week11/ressources/graph.java
+++ 2020/week11/ressources/graph.java
@@ -1,16 +1,16 @@
 public class graph {
-    // Les attributs de la class graph
+    // Les attributs de la class graphe
     public List<Edge> edges = new Vector();   // "Utilisation de vector car il faut que l'on puisse rajouter ou supprimer des éléments de la liste"
     public List<String> vertices = new Vector();
 
 
 
-    // Methode qui permet l'ajout d'un sommet au graph.
+    // "Methode qui permet l'ajout d'un sommet au graphe."
     public void add_vertex(String name){
-        this.vertices.add(name); // "Méthode qui permet d'ajouter un sommet au graph"
+        this.vertices.add(name); // "Méthode qui permet d'ajouter un sommet au graphe"
     }
 
-    // "Cette méthode va tester si le sommet demandé existe dans le graph. Si oui retourne le poids, sinon retourne 0."
+    // "Cette méthode va tester si le sommet demandé existe dans le graphe. Si oui retourne le poids, sinon retourne 0."
     public double edge_exist(String from_vertex, String to_vertex){
         for (Edge edge : this.edges) {
             if (edge.from_vertex == from_vertex & edge.to_vertex == to_vertex) {
@@ -20,8 +20,8 @@ public class graph {
         return 0;
     }
     // "L'implémentation de la méthode ci dessous n'est pas importante pour vous à comprendre. Elle vous est utile pour"
-    // "générer une arête lorsque vous cherchez à en ajouter une à votre graph. Elle fait aussi le test si jamais"
-    // "les sommets utilisés font partis du graph ou non. Si non, elle va les ajouter au graph. Cette méthode peut"
+    // "générer une arête lorsque vous cherchez à en ajouter une à votre graphe. Elle fait aussi le test si jamais"
+    // "les sommets utilisés font partis du graphe ou non. Si non, elle va les ajouter au graphe. Cette méthode peut"
     // "être utile dans la méthode new_edge"
     private void generate_edge(String from_vertex, String to_vertex, double weight){
         if (this.vertices.contains(from_vertex) & this.vertices.contains(to_vertex)){
--- 2020/week11/ressources/graph_empty.java
+++ 2020/week11/ressources/graph_empty.java
@@ -4,22 +4,22 @@ import java.util.Vector;
 
 public class graph_empty {
 
-    // Les attributs de la class graph
-    public List<Edges> edges = new Vector();   // Utilisation de vector car il faut que l'on puisse rajouter ou supprimer des éléments de la liste
+    // Les attributs de la classe graphe
+    public List<Edges> edges = new Vector();   // Utilisation de vector car il faut que l on puisse rajouter ou supprimer des éléments de la liste
     public List<String> vertices = new Vector();
     
-    // Methode qui permet l'ajout d'un sommet au graph.
+    // Methode qui permet l ajout d un sommet au graphe.
     public void add_vertex(String name){
-        this.vertices.add(name); // Méthode qui permet d'ajouter un sommet au graph
+        this.vertices.add(name); // Méthode qui permet d ajouter un sommet au graphe
     }
 
-    // Cette méthode va tester si le sommet demandé existe dans le graph. Si oui retourne le poids, sinon retourne 0.
+    // Cette méthode va tester si le sommet demandé existe dans le graphe. Si oui retourne le poids, sinon retourne 0.
     public double edge_exist(String from_vertex, String to_vertex){
         // Ecrire votre code ici
     }
-    // L'implémentation de la méthode ci dessous n'est pas importante pour vous à comprendre. Elle vous est utile pour
-    // générer une arête lorsque vous cherchez à en ajouter une à votre graph. Elle fait aussi le test si jamais
-    // les sommets utilisés font partis du graph ou non. Si non, elle va les ajouter au graph. Cette méthode peut
+    // L implémentation de la méthode ci dessous n est pas importante pour vous à comprendre. Elle vous est utile pour
+    // générer une arête lorsque vous cherchez à en ajouter une à votre graphe. Elle fait aussi le test si jamais
+    // les sommets utilisés font partis du graphe ou non. Si non, elle va les ajouter au graphe. Cette méthode peut
     // être utile dans la méthode new_edge
     private void generate_edge(String from_vertex, String to_vertex, double weight){
         if (this.vertices.contains(from_vertex) & this.vertices.contains(to_vertex)){
@@ -42,12 +42,12 @@ public class graph_empty {
     public void update_weight(String from_vertex, String to_vertex, double weight){
         // Ecrire votre code ici
     }
-    // Méthode qui va ajouter l'arête dans le graph.
+    // Méthode qui va ajouter l arête dans le graphe.
     public void new_edge(String from_vertex, String to_vertex, double weight){
         // Ecrire votre code ici
     }
 
-    // Méthode nous permettant de supprimer une arête du graph.
+    // Méthode nous permettant de supprimer une arête du graphe.
     public void del_edge(String from_vertex, String to_vertex){
         for(Edges edge : this.edges ){
             if (edge.from_vertex == from_vertex & edge.to_vertex == to_vertex){
@@ -58,7 +58,7 @@ public class graph_empty {
         }
     }
 
-    // Fonction qui permet d'imprimer les composants d'un graph
+    // Fonction qui permet d imprimer les composants d un graphe
     public void print(){
         for(int i=0; i< this.vertices.size(); ++i){
             System.out.println("The vertex number " + (i+1) + " has a value of: " + this.vertices.get(i));
