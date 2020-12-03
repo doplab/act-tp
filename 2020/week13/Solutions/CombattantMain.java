f001c4ff1704266b0569a821a7e40fdb4a2104f8
Gaëtan Michelet <55562103+guymono@users.noreply.github.com>
2020-12-03 18:10:29 +0100

    update

--- 2020/week13/Solutions/CombattantMain.java
+++ 2020/week13/Solutions/CombattantMain.java
@@ -1,21 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         Combattant P1 = new Combattant("P1", 10, 2, 2);
-        Combattant P2 = new Attaquant("P2", 10, 2, 2);
-        Combattant P3 =  new Attaquant("P3", 10, 2, 3);
+        Attaquant P2 = new Attaquant("P2", 10, 2, 2,2);
+        Soigneur P3 = new Soigneur("P3",10,2,2,4);
         P1.attack("pied",P2);
         P1.attack("poing",P2);
         P1.attack("tete",P2);
         P1.attack("tete",P2);
-        Soigneur P4 = new Soigneur("P4",10,2,2,4);
-        P4.résurection(P2);
-        P1.attack("pied",P4);
-        P1.attack("poing",P4);
-        P1.attack("tete",P4);
-        P1.attack("tete",P4);
-        P4.résurection(P2);
-        Soigneur P5 = new Soigneur("P5",10,2,2,4);
-        P5.soigne(P4);
-        P2.attack("tete",P2);
+        P3.résurection(P2);
+        P1.attack("pied",P2);
+        P1.attack("poing",P2);
+        P1.attack("tete",P2);
+        P3.attack(P2);
+        P2.attack("tete",P1);
     }
 }
