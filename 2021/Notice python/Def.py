0b5eedefc6aef67fa97948f5a294e0da6f6a9d79
ABonvin <89988280+ABonvin@users.noreply.github.com>
2021-09-13 21:51:49 +0200

    Notice python

---
+++ 2021/Notice python/Def.py
@@ -0,0 +1,4 @@
+def ma_fonction_perso():
+    print("Ma_fonction_est_super_!")
+
+ma_fonction_perso()
---
+++ 2021/Notice python/Variables.py
@@ -0,0 +1,11 @@
+x=12, aBcD=32, a="Alpha", b=True, c=x
+
+print("x vaut", x)
+print("aBcD vaut", aBcD)
+print("a vaut", a)
+print("b vaut", b)
+print("c vaut", x)
+
+x=99
+
+print("x vaut maintenant", x)
