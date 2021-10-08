7419ffc62c80fce4bd63768a356e66d7374f07dd
Alpha Diallo <alpha.diallo@unil.ch>
2021-10-08 10:00:27 +0200

    Slight changes

---
+++ 2021/week3_part1/updated/Question8.py
@@ -0,0 +1,8 @@
+# Programme 7
+variable_1 = 0
+variable_2 = "Hello"
+variable_2 = variable_1
+
+# Programme 8
+variable_1 = 0
+variable_1 = 3.14
--- sandbox/src/com/company/Main.java
+++ sandbox/src/com/company/Main.java
@@ -6,6 +6,14 @@ public class Main {
 	int a = 1;
 	double b = 2.12;
 	float c = 3.14151f;
+	boolean t = true;
+	boolean f = false;
+
+	System.out.println(t && f);
+	System.out.println(t || f);
+	System.out.println(t || !f);
+	System.out.println(!t || f);
+
 
     }
 }
