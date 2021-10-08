4d4dfceedd19eab9381f96ac0d161ed38c30c159
Alpha Diallo <alpha.diallo@unil.ch>
2021-10-08 10:49:14 +0200

    Week4 reviewed

--- 2021/week4/resources/question11.java
+++ 2021/week4/resources/question11.java
@@ -4,14 +4,14 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-        HashMap mon_dictionnaire = new HashMap(Map.of("étudiants",14000,"enseignants",2300,"collaborateurs",0));
+        HashMap<String, Integer> mon_dictionnaire = new HashMap<String, Integer>(Map.of("étudiants", 14000, "enseignants", 2300, "collaborateurs", 0));
         System.out.println(mon_dictionnaire.get("étudiants"));
         int taille_dictionnaire = mon_dictionnaire.size();
         System.out.println(taille_dictionnaire);
-        mon_dictionnaire.put("collaborateurs",950);
-        mon_dictionnaire.put("pays",86);
-        for (Object keys : mon_dictionnaire.keySet()){
-        System.out.println(keys + " : "+ mon_dictionnaire.get(keys));
+        mon_dictionnaire.put("collaborateurs", 950);
+        mon_dictionnaire.put("pays", 86);
+        for (Object keys : mon_dictionnaire.keySet()) {
+            System.out.println(keys + " : " + mon_dictionnaire.get(keys));
         }
     }
 }
