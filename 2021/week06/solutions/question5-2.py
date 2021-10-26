12f5a2c93577012440a7f200f818d10fb298fa84
Alpha Diallo <alpha.diallo@unil.ch>
2021-10-26 09:12:32 +0200

    Exercises week 6 - done!

--- 2021/week06/solutions/question5-2.py
+++ 2021/week06/solutions/question5-2.py
@@ -1,74 +1,24 @@
-################## Ne pas modifier le code ci-dessous ##################
-
-################## Générations des arbres en python ####################
-class Arbre:
-    def __init__(self, *args):
-        self.root = Node(args[0])
-        for i in range(1, len(args)):
-            self.insert(args[i], self.root)
-
-    def insert(self, value, node):
-        if node.value == value:
-            return
-        elif value < node.value:
-            if node.left is not None:
-                return self.insert(value, node.left)
-            new_node = Node(value)
-            node.left = new_node
+def recherche_binaire_iterative(liste,s,r,x):
+    while s < r:
+        mid = int(s + (r-s)/2)
+        print(f"La moitié correspond à {mid}")
+        # if x is present at mid
+        if liste[mid] == x:
+            print("X dans liste à l'index: ", mid)
+            return mid
+        # If x is greater, ignore left half
+        elif liste[mid] < x:
+            s = mid+1
+        # If x is smaller, ignore right half
         else:
-            if node.right is not None:
-                return self.insert(value, node.right)
-            new_node = Node(value)
-            node.right = new_node
+            r= mid-1
+    # If we reach here, then the element was not present
+    print("X absent de liste")
+    return -1
 
-    def __str__(self):
-        return str(self.root)
-
-
-def main():
-    a = Arbre([8, 6, 10, 2, 10, 3, 3, 23, 100, 323, 22])
-    print(a.root)
-
-
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
-
-    def __str__(self):
-        return "{value: %d, left: %s, right: %s}" % (self.value, self.left, self.right)
-
-
-################## Test de vérification du code ##################
-def handle_error(): #Fonction appelée dans le bloc "except" qui gère les erreurs
-    _, _, tb = sys.exc_info()
-    traceback.print_tb(tb)
-    tb_info = traceback.extract_tb(tb)
-    filename, line, func, text = tb_info[-1]
-    print('Une erreur s\'est produite sur la ligne {} dans la déclaration {}'.format(line, text))
-
-try: #"try" permet de tester le code pour détecter les erreurs.
-    tree = Arbre(8, 3, 1, 6, 4, 7, 10, 14, 13)
-    root = tree.root
-    assert recherche_arbre(root, 4) == True # devrait trouver
-    assert recherche_arbre(root, 7) == True # devrait trouver
-    assert recherche_arbre(root, 18) == False # ne devrait pas trouver
-    assert recherche_arbre(root, 21) == False # ne devrait pas trouver
-
-    print("Bonne réponse avec le premier arbre")
-except AssertionError: #"except" permet de gérer l'erreur
-    handle_error()
-    print("Mauvaise réponse")
-
-try:
-    tree = Arbre(1, 3, 9, 6, 14, -17, 110, 124, 13, -1, 9, 1, 40, -98, 120, 23)
-    root = tree.root
-    assert recherche_arbre(root, 14) == True # devrait trouver
-    assert recherche_arbre(root, 110) == True # devrait trouver
-    assert recherche_arbre(root, 39) == False # ne devrait pas trouver
-    assert recherche_arbre(root, 18) == False # ne devrait pas trouver
-    print("Bonne réponse avec le deuxième arbre")
-except AssertionError as err:
-    handle_error()
-    print("Mauvaise réponse")
+L = [1,3,4,5,7,8,9,15]
+s = 0
+r = len(L)
+print(f"La liste contient {r} éléments")
+x = 6
+recherche_binaire_iterative(L,s, r, x)
