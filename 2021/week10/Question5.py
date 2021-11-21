f730dd557b2e7d122516d207bbfde68c2585f19f
Nathan Ballèvre <63353024+pastouche2@users.noreply.github.com>
2021-11-21 17:44:46 +0100

    Add files via upload

---
+++ 2021/week10/Question5.py
@@ -0,0 +1,13 @@
+#Question 5
+
+root = [(0,10), None, None] #Nous définissons ici juste la racine de l'arbre
+k = 2 # Ici nous travaillerons en 2 dimensions
+point = (-10,0) #Point que nous voulons ajouter dans le graphe
+
+def add_node(node,point, cutaxis = 0):
+    #Codez ici !
+    return Node
+
+add_node(root,point)
+
+print(root)
