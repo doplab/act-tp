625ffb9fa0533ab3b4c67fe5b7ee55fc01e066c3
Alpha Diallo <alpha.diallo@unil.ch>
2021-11-23 09:24:19 +0100

    TP10 done

--- 2021/week10/Solutions/Question1_solution.py
+++ 2021/week10/Solutions/Question1_solution.py
@@ -1,6 +1,5 @@
-#Time to code !
-import math #permet d'importer la library nécessaire au calcul de la racine carrée
+import math #permet d'importer la librairie nécessaire au calcul de la racine carrée
 
 def calculate_distance(point1,point2):
-    #Implémentez la formule de la distance euclidienne entre 2 point ici
+    #Cette fonction retourne la distance euclidienne entre 2 points
     return math.sqrt((point1[0]-point2[0])**2+(point1[1]-point2[1])**2)
--- 2021/week10/Solutions/Question2_solution.py
+++ 2021/week10/Solutions/Question2_solution.py
@@ -1,20 +1,27 @@
-#Question 2
-
-def nearest_neighbor(start, point_set):#start correspond au point de départ, point_set correspond à l'ensemble des points
-    for i in range(len(point_set)):#on parcourt tout les point de l'ensemble
-        
+def nearest_neighbor(start, point_set):  # start correspond au point de départ, point_set correspond 
+                                                                    # à l'ensemble des points
+    min_distance = None
+    for i in range(len(point_set)):  # on parcourt tout les point de l'ensemble
         if i == 0:
+            # La distance minimale n'étant pas définie, on doit l'initialiser à la première itération, c'est ce qu'on
+            # fait ici
             min_distance = calculate_distance(start, point_set[0])
             nearest_nei = point_set[0]
-            #La distance minimale n'étant pas définie, on doit l'initialiser à la première itération, c'est ce qu'on fait ici
-            
+
         distance = calculate_distance(start, point_set[i])
-        
-        if distance < min_distance:            
+        if distance < min_distance:
             min_distance = distance
-            nearest_nei = point_set[i]   
-            
-        #Cette partie du code détermine si le point actuellement considéré, est plus proche du point de départ que les points 
-        #parcouru jusqu'ici. Si c'est le cas, on redéfinit la distance minimale et "enregistre" les coordonées du point
-        
+            nearest_nei = point_set[i]
+
+            # Cette partie du code détermine si le point actuellement considéré, est plus proche du point de départ que les points
+        # parcourus jusqu'ici. Si c'est le cas, on redéfinit la distance minimale et on "enregistre" les coordonnées du point
+
     return nearest_nei, min_distance
+
+
+a = [(1, 2), (5, 6), (7, 8), (2, 5), (9, 1)]  # Liste de points
+b = (3, 4)  # Point de départ
+
+point, distance = nearest_neighbor(b, a)
+print(f'{point}, {distance}')
+# Devrait retourner (2, 5), 1.4142135623730951
