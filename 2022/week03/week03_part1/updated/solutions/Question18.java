650a4d6e301bbdaf6374b593fdd0e02756b682c1
Alpha Diallo <alpha.diallo@unil.ch>
2022-10-03 12:17:30 +0200

    TP3 reviewed

--- 2022/week03/week03_part1/updated/solutions/Question18.java
+++
@@ -1,5 +0,0 @@
-String mon_mot = "Hard But Cool !!";
-String lettre_5 = mon_mot.substring(4,5);
-String lettre_9_13 = mon_mot.substring(8,13);
-System.out.println(lettre_5);
-System.out.println(lettre_9_13);
