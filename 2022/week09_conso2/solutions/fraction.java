04205c34088f9d916f66d6b78416676ed536786e
Alpha Diallo <alphadiallo009@gmail.com>
2022-11-14 17:21:05 +0100

    Question on identity vs equality in Java

---
+++ 2022/week09_conso2/solutions/fraction.java
@@ -0,0 +1,38 @@
+public class Fraction {
+    public int numerateur;
+    public int denominateur;
+
+    public Fraction(int numerateur, int denominateur){
+        this.numerateur = numerateur;
+        this.denominateur = denominateur;
+        simplify();
+    }
+
+    public int gcd(int a, int b){
+        if (a==0){
+            return b;
+        } else if (b==0) {
+            return a;
+        } else{
+            return gcd(b%a, a);
+        }
+    }
+
+    private void simplify(){
+        int pgcd = gcd(this.numerateur, this.denominateur);
+        this.numerateur = this.numerateur / pgcd;
+        this.denominateur = this.denominateur / pgcd;
+    }
+
+    @Override
+    public String toString() {
+        return numerateur + "/" + denominateur;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other == null || this.getClass() != other.getClass()) return false;
+        return this.numerateur * ((Fraction) other).denominateur == ((Fraction) other).numerateur * this.denominateur;
+    }
+}
