09c990fc719ff064807193d487eac49a4ab8a014
Alpha Diallo <alphadiallo009@gmail.com>
2022-11-27 19:25:28 +0100

    Exercises week11

---
+++ 2022/week11/solutions/question1_advanced.py
@@ -0,0 +1,20 @@
+def recherche_binaire(liste,e):
+    first = 0 #correspond à l'index du premier élément de la liste
+    last = len(liste) #correspond à l'index du dernier élément de la liste
+    #SOLUTION
+    while first <= last:
+        mid = int((first+last)/2) #l'élément du milieu. La fonction int() permet d'obtenir un entier dans le cas ou "first + last" est un nombre impair
+        print(mid)
+        if liste[mid] == e:
+            return mid #Si la condition est juste, la fonction retourne l'index de la valeur recherchée dans la liste
+        else:
+            if liste[mid] > e:
+                last = mid-1
+            else:
+                first = mid+1
+    return -1 # Si la condition de la ligne 8 n'est jamais remplie, la fonction retourne -1 
+
+L = [123, 321, 328, 472, 549]
+e = 328
+recherche_binaire(L,e)
+
