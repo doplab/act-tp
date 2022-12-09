242a26a5952527c8eec956af3011ba920c6995da
Hafsa Aoutir <hafsaa@kth.se>
2022-12-09 18:56:21 +0100

    Fixing typos week 13

---
+++ 2022/week13/resources/Question5.py
@@ -0,0 +1,13 @@
+#Question 5
+
+root = [(0,10), None, None] #Nous définissons ici juste la racine de l'arbre
+k = 2 # Ici nous travaillerons en 2 dimensions
+point = (-10,0) #Point que nous voulons ajouter dans le graphe
+
+def add_node(node,point,k, cutaxis = 0):
+    #Codez ici !
+    return node
+
+add_node(root,point)
+
+print(root)
