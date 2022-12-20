cd5bfcb2bfd5f5998ae9773aa9444ec99e96a431
Alpha Diallo <alpha.diallo@unil.ch>
2022-12-20 08:14:16 +0100

    Update Piece.py

--- 2022/week14/resources/Piece.py
+++ 2022/week14/resources/Piece.py
@@ -1,6 +1,6 @@
 import random
 
-#La fonction Piece retourn une liste contenant des 0 et des 1, considérez un 1 comme un succès, i.e. une fois ou la pièce tombe sur pile, et 0 comme un échec
+# La fonction Piece retourne une liste contenant des 0 et des 1, considérez un 1 comme un succès, i.e. une fois ou la pièce tombe sur pile, et 0 comme un échec
 def Piece(l):
     return [random.randint(0, 1) for i in range(l)]
 
