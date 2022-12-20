b28fc0f044b445e58abd2a06136e22f6c1016fad
Alpha Diallo <alphadiallo009@gmail.com>
2022-12-20 11:19:46 +0100

    Fingerprinting reviewed

--- 2022/week14/solutions/fingerprinting_solution.py
+++ 2022/week14/solutions/fingerprinting_solution.py
@@ -16,8 +16,8 @@ def fingerprinting(p, message):
         return result
     print(str(p) + " is not a prime number!")
 
-# password est une chaîne de caractères et your details est un tuple avec le 
-# format suivant (nombre premier, hash du mot de passe)
+# password est une chaîne de caractères et your details est un tuple 
+# avec le format suivant (nombre premier, hash du mot de passe)
 def login(password, your_details):
     return your_details[1] % your_details[0] == fingerprinting(your_details[0], password)
 
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     print("Connexion réussie? " + str(success))
     if success:
         message = '''SmUgc2VyYWlzIGNvbmZpbsOpIGNoZXogbWVzIHBhcmVudHMgw
-                     6AgbGEgY2FtcGFnbmUgbGVzIGRldXggcHJvY2hhaW5lIHNlbWF
-                     pbmVzLCBldCBqZSBuJ2F1cmFpcyBwYXMgYWNjw6hzIMOgIEludGV
-                     ybmV0LiDDgCBiaWVudMO0dCE='''
+                    6AgbGEgY2FtcGFnbmUgbGVzIGRldXggcHJvY2hhaW5lIHNlbWF
+                    pbmVzLCBldCBqZSBuJ2F1cmFpcyBwYXMgYWNjw6hzIMOgIElu
+                    dGVybmV0LiDDgCBiaWVudMO0dCE='''
         print(base64.b64decode(message).decode())
