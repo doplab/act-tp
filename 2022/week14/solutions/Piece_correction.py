399a7b1c517162bc0ad14ecd43ac2b4bf8739f39
Alpha Diallo <alphadiallo009@gmail.com>
2022-12-20 11:35:55 +0100

    Function name and variable have the same names

--- 2022/week14/solutions/Piece_correction.py
+++ 2022/week14/solutions/Piece_correction.py
@@ -10,15 +10,15 @@ def proba(n, l, iter=10000):
     # n correspond au nombre de succès et l au nombre d'essais. Iter correspond au nombre d'expérience que vous allez
     # réaliser pour obtenir la réponse. Cela devrait être grand mais pas trop (sinon le programme prendra trop de
     # temps à s'exécuter). 10000 est un bon nombre d'itérations.
-    proba = 0
+    occurences = 0
     for i in range(iter):
         temp = Piece(l)  # On simule une expérience de l lancés.
         count = sum(temp)  # On compte le nombre de fois que l'on obtient pile
         if count == n:  # Si le nombre de pile obtenu correspond à la probabilité que l'on veut estimer
-            proba += 1  # On ajoute 1 à notre estimateur de probabilité
-    return proba / iter  # Divise notre estimateur de probabilité par le nombre total d'expériences réalisées.
+            occurences += 1  # On ajoute 1 à notre estimateur de probabilité
+    return occurences / iter  # Divise notre estimateur de probabilité par le nombre total d'expériences réalisées.
 
 
 n = 5
 l = 10
-print("La probabilité d'avoir {} pile en {} lancés de pièce est approximativement égale à {}".format(n, l, proba(n, l,10000)))
+print("La probabilité d'avoir {} pile en {} lancers de pièce est approximativement égale à {}".format(n, l, proba(n, l,10000)))
