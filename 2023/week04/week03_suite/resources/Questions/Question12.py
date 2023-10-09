cc0e0784a8a9df2bb29cb1df9b476a3ab4a9d474
Alpha Diallo <alpha.diallo@unil.ch>
2023-10-09 14:15:39 +0200

    TP3 suite updated

--- 2023/week04/week03_suite/resources/Questions/Question12.py
+++ 2023/week04/week03_suite/resources/Questions/Question12.py
@@ -4,11 +4,11 @@ lesson = "ACT"
 
 if ((teacher == "Diallo") or not(teacher == "Garbinato")) and (lesson == "ACT"):
     print("Alpha Diallo et Benoît Garbinato")
-elif ((teacher == "Benfares") and (lesson == "ACT")) or (teacher == assistant):
-    print("Anass Benfares")
-elif (teacher == "Gaelle" or assistant == "Diallo") and (lesson == "ACT" and teacher == "Julien"):
-    print("Gaelle Barillère")
-elif ((teacher == "Garbinato" or lesson == "INF") and assistant == "Diallo") or (lesson == "ACT" or teacher == "Hafsa"):
-    print(teacher + " : Professeur du cours Algorithmique et pensée computationelle.")
+elif ((teacher == "George") and (lesson == "ACT")) or (teacher == assistant):
+    print("Amaury George")
+elif (teacher == "Mathieu" or assistant == "Diallo") and (lesson == "ACT" and teacher == "Marc"):
+    print("Marc Pitteloud")
+elif ((teacher == "Garbinato" or lesson == "INF") and assistant == "Diallo") or (lesson == "ACT" or teacher == "Valentin"):
+    print(teacher + " : Professeur du cours d'Algorithmes et Pensée Computationnelle.")
 else:
     print("Benoît")
