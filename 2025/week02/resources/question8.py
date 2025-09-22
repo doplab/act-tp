8ed210fde8c0d00ec93a06430c5cc627a78a6b1d
Amro Abdrabo <amro.abdrabo@gmail.com>
2025-09-22 13:50:26 +0200

    update TP to include better indenting for code, change to f-strings, bash in powershell, corrected standard library in python

--- 2025/week02/resources/question8.py
+++ 2025/week02/resources/question8.py
@@ -1,8 +1,8 @@
 import sys
 
-if __name__ == '__main__':
-  arguments = sys.argv
-  if len(arguments) == 3:
-    print("La somme de {} et {} est {}".format(sys.argv[1], sys.argv[2], int(sys.argv[1])+int(sys.argv[2])))
-  else:
-    print("Assurez-vous de passer deux arguments à votre programme")
+if __name__ == "__main__":
+    arguments = sys.argv
+    if len(arguments) == 3:
+        print(f"La somme de {sys.argv[1]} et {sys.argv[2]} est {int(sys.argv[1]) + int(sys.argv[2])}")
+    else:
+        print("Assurez-vous de passer deux arguments à votre programme")
