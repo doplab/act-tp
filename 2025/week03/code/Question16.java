dddfedc1f048dd4f3cdc58f9ea66d5c4cff04715
Amro Abdrabo <amro.abdrabo@gmail.com>
2025-10-03 11:00:33 +0200

    fix typos

--- 2025/week03/code/Question16.java
+++ 2025/week03/code/Question16.java
@@ -8,7 +8,7 @@ public class Question16 {
         System.out.println(s.substring(5, 20));
 
         // Accéder au troisième caractère
-        System.out.println(s.charAt(2)); // 'h'
+        System.out.println(s.charAt(2)); // 'I'
 
         System.out.println(s.length());
     }
