77ae3c932fded2dc7094417234df299d44061693
Amro Abdrabo <amro.abdrabo@gmail.com>
2025-10-05 19:11:50 +0200

    modify last question 11 of tp4

--- 2025/week04/ressources/Questions/question11.py
+++ 2025/week04/ressources/Questions/question11.py
@@ -2,7 +2,14 @@ import sys
 if __name__ == '__main__':
     balance = 20
     try:
-        balance = 20 - sys.argv[1]
+        balance = balance - sys.argv[1]
+        solde_post = balance > 0
+        match (solde_post):
+            case True:
+                print('Le solde de votre compte est positif.')
+            case False:
+                print('Le solde de votre compte est négatif ou zéro.')
+                raise ValueError('Le compte ne peut pas être débité')
     except Exception as e:
         print("Une erreur est survenue")
     finally:
