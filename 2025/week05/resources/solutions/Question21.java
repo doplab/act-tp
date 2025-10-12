b3cd09d951cce5590cc1c6f1b4182eef941c0554
Amro Abdrabo <amro.abdrabo@gmail.com>
2025-10-12 16:57:13 +0200

    binary represention exercice to tp5

---
+++ 2025/week05/resources/solutions/Question21.java
@@ -0,0 +1,16 @@
+public class Question21 {
+    public static void main(String[] args) {
+        // Essayez d'exécuter avec un argument de la ligne de commande
+        System.out.println(getBinaryRepresentation(args[0]));
+    }
+
+    public static String getBinaryRepresentation(String arg) {
+        int a = Integer.parseInt(arg);
+        if (a == 0) {
+            return "";
+        }
+        int b = a / 2;
+        int r = a % 2;
+        return getBinaryRepresentation(String.valueOf(b)) + String.valueOf(r);
+    }
+}
