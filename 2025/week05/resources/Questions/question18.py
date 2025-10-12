6305216d03de2be1a4fcee2f3ed59cb53b98ceee
Amro Abdrabo <amro.abdrabo@gmail.com>
2025-10-12 11:18:02 +0200

    add tp5

---
+++ 2025/week05/resources/Questions/question18.py
@@ -0,0 +1,8 @@
+def fonction_p(x) :
+    print(x)
+    if x == 0 :
+        pass
+    else :
+        fonction_p(x-1)
+
+fonction_p(5)
---
+++ 2025/week05/resources/question2.py
@@ -0,0 +1,3 @@
+mon_tuple = (1,2,3,4,5)
+taille_tuple = len(mon_tuple)
+print(taille_tuple)
