9ad26e3ba0b89c33b49acf0037ed6bd6a013f8d9
RedaMehdaoui <mohamed.mehdaoui@epfl.ch>
2025-10-21 12:07:24 +0200

    Reordering the lines of code and remove line returns to be in accordance with the print expectation of question 11 tp6 basique

--- 2025/week06/ressources/heritage-main.java
+++ 2025/week06/ressources/heritage-main.java
@@ -3,8 +3,8 @@ public class Main {
     public static void main(String[] args) {
         Livre Livre1 = new Livre_Audio("Hamlet", "Shakespeare", 1609,"William");
         Livre1.setNote(5);
-        System.out.println(Livre1);
         Livre Livre2 = new Livre("Les Misérables","Hugo",1862);
-        System.out.println(Livre2);
+        System.out.print(Livre1);
+        System.out.print(Livre2);
     }
 }
