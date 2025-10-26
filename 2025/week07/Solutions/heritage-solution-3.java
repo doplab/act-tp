da0419098c90cd8ee12b3e221dfe2072f97d333a
Amro Abdrabo <amro.abdrabo@gmail.com>
2025-10-26 19:27:50 +0100

    tp7

---
+++ 2025/week07/Solutions/heritage-solution-3.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Livre Livre1 = new Livre_Audio("Hamlet", "Shakespeare", 1609,"William");
+        Livre1.setNote(5);
+        System.out.println(Livre1);
+    }
+}
