cd81ace92759bf5ccc372addd6e7c31d323b4b02
Amro Abdrabo <amro.abdrabo@gmail.com>
2025-10-26 21:42:46 +0100

    point3d protected not private attributes

--- 2025/week07/Solutions/Point3D.py
+++ 2025/week07/Solutions/Point3D.py
@@ -1,44 +1,44 @@
 class Point3D(Point):
     def __init__(self, x, y, z):
         super().__init__(x, y)
-        self.__z = z
+        self._z = z
         
     def get_z(self):
-        return self.__z
+        return self._z
     
     def set_z(self, z):
-        self.__z = z
+        self._z = z
         
     def vector_representation(self): # représentée sous forme de liste
-        return [self.__x, self.__y, self.__z]
+        return [self._x, self._y, self._z]
         
     def distance_euclidean(self, p2): # i.e norme
         other_x = p2.get_x()
         other_y = p2.get_y()
         other_z = p2.get_z()
-        return math.sqrt((self.__x - other_x)**2 + (self.__y - other_y)**2 + (self.__z - other_z)**2)
+        return math.sqrt((self._x - other_x)**2 + (self._y - other_y)**2 + (self._z - other_z)**2)
     
     def distance_manhattan(self, p2):
         other_x = p2.get_x()
         other_y = p2.get_y()
         other_z = p2.get_z()
-        return sum((abs(self.__x - other_x), abs()), abs(self.__y - other_y), abs(self.__z - other_z))
+        return sum((abs(self._x - other_x), abs()), abs(self._y - other_y), abs(self._z - other_z))
         
     def distance_minkowski(self, p2, order=3):
         other_x = p2.get_x()
         other_y = p2.get_y()
         other_z = p2.get_z()
-        return sum((abs(self.__x - other_x)**order), abs(self.__y - other_y)**order,\
-                   abs(self.__z - other_z)**order)**(1/order)
+        return sum((abs(self._x - other_x)**order), abs(self._y - other_y)**order,\
+                   abs(self._z - other_z)**order)**(1/order)
         
     def milieu(self, p2):
         other_x = p2.get_x()
         other_y = p2.get_y()
         other_z = p2.get_z()
         
-        x_M = (self.__x + other_x)/2
-        y_M = (self.__y + other_y)/2
-        z_M = (self.__z + other_z)/2
+        x_M = (self._x + other_x)/2
+        y_M = (self._y + other_y)/2
+        z_M = (self._z + other_z)/2
         return Point3D(x_M, y_M, z_M) # renvoie un point!
         
         
