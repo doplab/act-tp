c06d3e5be5d43ddd63e45ad653690e7b2bfb0396
Perazzolo Sean <sean.perazzolo@gmail.com>
2025-11-21 11:01:45 +0100

    q2 format + typos + list->liste

--- 2025/week11/resources/question2.py
+++ 2025/week11/resources/question2.py
@@ -1,15 +1,17 @@
-#Definition de la fonction ayant pour argument une liste et un nombre
-def plus_proche_sequentielle(list,nb):
-    diff = -1 #Initialisation de la variable (-1 car les différence calculée après seront toujours positives)
-    resultat = None #Initialisation de la variable pour le résultat
+# Definition de la fonction ayant pour argument une liste et un nombre
+def plus_proche_sequentielle(liste, nb):
+    # Initialisation de la variable (-1 car les différence calculée après seront toujours positives)
+    diff = -1
+    resultat = None  # Initialisation de la variable pour le résultat
 
-    #Complètez ici
+    # Complètez ici
+
 
 if __name__ == '__main__':
-    #Déclaration de la liste et de la variable e
+    # Déclaration de la liste et de la variable e
     L = [16, 2, 25, 8, 12, 31, 2, 56, 58, 63]
     e = 50
 
-    #Exécution de la fonction
-    resultat = plus_proche_sequentielle(L,e)
+    # Exécution de la fonction
+    resultat = plus_proche_sequentielle(L, e)
     print(resultat)
