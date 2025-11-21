f1ac15151c5c13b04014da6a1b9bcc5d7eadd649
Perazzolo Sean <sean.perazzolo@gmail.com>
2025-11-21 11:04:48 +0100

    sol/q2 format + french typos

--- 2025/week11/solutions/question2.py
+++ 2025/week11/solutions/question2.py
@@ -1,22 +1,24 @@
-#Definition de la fonction ayant pour argument une liste et un nombre
-def plus_proche_sequentielle(liste,nb):
-    diff = -1 #Initialisation de la variable ( -1 car les différences calculées après seront toujours positives)
-    resultat = None #Initialisation de la variable pour le résultat
+# Définition de la fonction ayant pour argument une liste et un nombre
+def plus_proche_sequentielle(liste, nb):
+    # Initialisation de la variable ( -1 car les différences calculées après seront toujours positives)
+    diff = -1
+    resultat = None  # Initialisation de la variable pour le résultat
 
-    #Solution
+    # Solution
     for elem in liste:
         if diff == -1 or abs(elem-nb) < diff:
-            diff = abs(elem-nb) #new diff #
+            diff = abs(elem-nb)  # nouvelle diff.
             resultat = elem
         elif (abs(elem-nb) == diff):
-            resultat=min(elem, resultat)
+            resultat = min(elem, resultat)
 
     return resultat
+
 
 # Déclaration et initialisation de la liste L et de la variable e
 L = [16, 2, 25, 8, 12, 31, 2, 56, 58, 63]
 e = 50
 
 # Exécution de la fonction
-resultat = plus_proche_sequentielle(L,e)
-print(resultat) # affiche 56
+resultat = plus_proche_sequentielle(L, e)
+print(resultat)  # affiche 56
