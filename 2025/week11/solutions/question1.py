9e74f756bd5114cb824aa1d97e3c5104d15cba32
Perazzolo Sean <sean.perazzolo@gmail.com>
2025-11-21 10:45:10 +0100

    python format: sol/q1.py

--- 2025/week11/solutions/question1.py
+++ 2025/week11/solutions/question1.py
@@ -1,4 +1,4 @@
-#Définition de la fonction
+# Définition de la fonction
 def recherche_sequentielle(liste, x):
     for index in range(len(liste)):  # i représente l'index
         if liste[index] == x:
@@ -8,11 +8,13 @@ def recherche_sequentielle(liste, x):
     print("X n'est pas présent dans la liste")
     return -1
 
+
 if __name__ == '__main__':
     # Déclaration de la liste et de la variable x
-    L = [3,55,6,8,3,5,56,33,6,5,3,2,99,53,532,75,21,963,100,445,56,56,24]
+    L = [3, 55, 6, 8, 3, 5, 56, 33, 6, 5, 3, 2, 99,
+         53, 532, 75, 21, 963, 100, 445, 56, 56, 24]
     e = 100
 
-    # Appel de la fonction 
-    resultat = recherche_sequentielle(L,e)
+    # Appel de la fonction
+    resultat = recherche_sequentielle(L, e)
     print(resultat)
