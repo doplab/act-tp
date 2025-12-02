547c8aa44f007af048ffc52e5f154f355536f540
Amro Abdrabo <amro.abdrabo@gmail.com>
2025-12-02 09:35:04 +0100

    update q2 and q6

--- 2025/week12/solutions/Question2.py
+++ 2025/week12/solutions/Question2.py
@@ -1,4 +1,4 @@
-#Question 2
+# Question 2
 
 adjacency_list_graph = {
   'A' : ['B','C'],
@@ -9,31 +9,21 @@ adjacency_list_graph = {
   'F' : []
 }
 
-def BFS(graph, start):
-    visited = list() # liste des sommets visités
-    queue = [start] # liste des sommets à visiter
+def bfs(graph, start):
+    visited = list(start) # liste des sommets visités
+    queue = [start] # liste des sommets *dont les voisins* sont à visiter
+    order = [] # pour stocker l'ordre de sommets (sommets proches d'abord puis les sommets qui sont plus loin)
     while len(queue) > 0: # tant que la queue n'est pas vide
         u = queue.pop(0) # on stocke le premier élément de la queue, puis on l'enlève de la queue
-        if u not in visited: # si le sommet n'a pas déjà été visité
-            visited.append(u) # on l'ajoute à la liste des sommets visités
+        order.append(u)
         neighbors = graph[u] # on récupère la liste des sommets adjacents au sommet courant
         for v in neighbors: # Puis on parcourt la liste de ceux-ci
             if v not in visited:
-                visited.append(v)
                 queue.append(v) # on les ajoute à la queue
-    return visited # on retourne la liste des sommets visités (=atteignables)
-    visited = list() # liste des sommets visités
-    queue = [start] # liste des sommets à visiter
-    while len(queue) > 0: # tant que la queue n'est pas vide
-        node = queue.pop(0) # on stocke le premier élément de la queue, puis on l'enlève de la queue
-        if node not in visited: # si le sommet n'a pas déjà été visité
-            visited.append(node) # on l'ajoute à la liste des sommets visités
-            neighbors = graph[node] # on récupère la liste des sommets adjacents au sommet courant
-            for neighbor in neighbors: # Puis on parcourt la liste de ceux-ci
-                queue.append(neighbor) # on les ajoute à la queue
-    return visited # on retourne la liste des sommets visités (=atteignables)
+                visited.append(v)
+    return order # on retourne la liste des sommets visités (=atteignables)
 
 
 # Vérifions que l'algorithme fonctionne correctement
-print(BFS(adjacency_list_graph, 'B'))
-print(BFS(adjacency_list_graph, 'A'))
+print(bfs(adjacency_list_graph, 'B'))
+print(bfs(adjacency_list_graph, 'A'))
