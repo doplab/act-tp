9f3bfdfff20f58f2dfd325d251ccd7cc9eac6de2
Amro Abdrabo <amro.abdrabo@gmail.com>
2025-12-08 12:28:17 +0100

    tp13 ready

--- 2025/week13/resources/Question6.py
+++ 2025/week13/resources/Question6.py
@@ -1,13 +1,16 @@
 # Question 6
 
-root = [(0,10), None, None] #Nous définissons ici juste la racine de l'arbre
-k = 2 # Ici nous travaillerons en 2 dimensions
-point = (-10,0) #Point que nous voulons ajouter dans le graphe
-
-def add_node(node,point,k, cutaxis = 0):
-    #Complétez cette fonction!
+def add_node(node, point, k, cutaxis=0):
+    # Complétez cette fonction
     return node
 
-add_node(root,point)
-
-print(root)
+if __name__ == "__main__":
+    # Nous définissons ici juste la racine de l'arbre
+    root = [(0,10), None, None]
+    k = 2  # Nous travaillons en 2 dimensions
+    
+    # Point que nous voulons ajouter dans l'arbre
+    point = (-10,0)
+    
+    add_node(root, point, k)
+    print(root)
