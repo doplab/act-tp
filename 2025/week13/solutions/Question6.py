745818684a94c0e9e610c9a73685617e528b0756
Amro Abdrabo <amro.abdrabo@gmail.com>
2025-12-08 12:02:08 +0100

    update tp13

--- 2025/week13/solutions/Question6.py
+++ 2025/week13/solutions/Question6.py
@@ -1,22 +1,28 @@
 # Question 6
 
-def add_node(node,point,k,cutaxis = 0):
-    
-    if node is None: #Si le noeud n'existe pas, nous sommes donc dans une feuille, et il faut créer le noeud
-        node = [point,None,None]
+def add_node(node, point, k, cutaxis=0):
+    # Si le noeud n'existe pas, nous sommes donc dans une feuille, et il faut créer le noeud
+    if node is None:
+        node = [point, None, None]
         return node
     
+    # (cutaxis + 1) % k ensures correct cycle 0 -> 1 -> 0 -> 1...
+    next_axis = (cutaxis + 1) % k
+
     if point[cutaxis] <= node[0][cutaxis]:
-        node[1] = add_node(node[1], point, k, cutaxis + 1 % k) 
-        
+        node[1] = add_node(node[1], point, k, next_axis)
     else:
-        node[2] = add_node(node[2], point, k, cutaxis + 1 % k)
+        node[2] = add_node(node[2], point, k, next_axis)
         
     return node
 
-
-root = [(0,10), None, None] #Nous définissons ici juste la racine de l'arbre
-k = 2 # Ici nous travaillerons en 2 dimensions
-point = (-10,0) #Point que nous voulons ajouter dans le graphe
-add_node(root,point,k)
-print(root)
+if __name__ == "__main__":
+    # Nous définissons ici juste la racine de l'arbre
+    root = [(0,10), None, None]
+    k = 2  # Nous travaillons en 2 dimensions
+    
+    # Point que nous voulons ajouter dans l'arbre
+    point = (-10,0)
+    
+    add_node(root, point, k)
+    print(root)
