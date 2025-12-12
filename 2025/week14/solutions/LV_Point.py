4d7db56d6207cc30a040c1984670d71b8552e6df
Perazzolo Sean <sean.perazzolo@gmail.com>
2025-12-12 12:01:05 +0100

    better lv point solution

--- 2025/week14/solutions/LV_Point.py
+++ 2025/week14/solutions/LV_Point.py
@@ -3,21 +3,18 @@ import random
 
 def inside(point):  # Point est défini sous la forme d'un tuple
     # Cette fonction permet de vérifier si un point se trouve à l'intérieur du cercle
-    if (point[0] ** 2 + point[1] ** 2) < 1:
-        return 1
-
-    else:
-        return 0
+    return (point[0] ** 2 + point[1] ** 2) < 1
 
 
 def app():
-    for i in range(10000):
-        temp1 = random.random()  # Génère la première coordonnée
-        temp2 = random.random()  # Génère la deuxième coordonnée
-        temp = (temp1, temp2)  # Crée le point
+    while True:
+        x = random.uniform(-1, 1)  # Génère la première coordonnée
+        y = random.uniform(-1, 1)  # Génère la deuxième coordonnée
+        p = (x, y)  # Crée le point
 
-        if (inside(temp)) :
-            return temp # Retourne le point trouvé.
+        if inside(p):
+            return p  # Retourne le point trouvé.
 
 
 print("Voilà un point dans un cercle unitaire  : {}".format(app()))
+
