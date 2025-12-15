af70d4d5fc2eae2606363044260d7268eeb7475e
rrluder <rrluder@gmail.com>
2025-12-15 20:23:03 +0100

    flip + temp + a couple things

--- 2025/week14/resources/Piece.py
+++ 2025/week14/resources/Piece.py
@@ -1,8 +1,8 @@
 import random
 
-# La fonction Piece retourne une liste contenant des 0 et des 1, considérez un 1 comme un succès, i.e. une fois ou la pièce tombe sur pile, et 0 comme un échec
-def piece(iter):
-    return [random.randint(0, 1) for i in range(iter)]
+# La fonction flip(l) retourne une liste de longueur l contenant des 0 et des 1, considérez un 1 comme un succès, i.e. une fois ou la pièce tombe sur pile, et 0 comme un échec.
+def flip(l):
+    return [random.randint(0, 1) for i in range(l)]
 
 def proba(n,k,iter):
     # Complétez ici
@@ -12,4 +12,4 @@ def proba(n,k,iter):
 if __name__ == '__main__':
     n = 10
     k = 5
-    print(f"La probabilité d'avoir {k} pile en {n} lancés de pièce est approximativement égale à {proba(n, k, 10000)}")
+    print(f"La probabilité d'avoir {k} piles en {n} lancés de pièce est approximativement égale à {proba(n, k, 10000)}")
--- 2025/week14/solutions/piece.py
+++ 2025/week14/solutions/piece.py
@@ -1,6 +1,6 @@
 import random
 
-# La fonction Piece retourne une liste contenant des 0 et des 1, considérez un 1 comme un succès, i.e. une fois où la pièce tombe sur pile, et 0 comme un échec
+# La fonction flip(l) retourne une liste de longueur l contenant des 0 et des 1, considérez un 1 comme un succès, i.e. une fois où la pièce tombe sur pile, et 0 comme un échec.
 
 def piece(l):
     return [random.randint(0, 1) for i in range(l)]
@@ -12,8 +12,8 @@ def proba(n, l, iter=10000):
     # temps à s'exécuter). 10000 est un bon nombre d'itérations.
     occurences = 0
     for i in range(iter):
-        flips = Flip(l)  # On simule une expérience de l lancers.
-        count = sum(temp)  # On compte le nombre de fois que l'on obtient pile
+        flips = flip(l)  # On simule une expérience de l lancers.
+        count = sum(flips)  # On compte le nombre de fois que l'on obtient pile
         if count == n:  # Si le nombre de piles obtenu correspond à la probabilité que l'on veut estimer
             occurences += 1  # On ajoute 1 à notre estimateur de probabilité
     return occurences / iter  # Divise notre estimateur de probabilité par le nombre total d'expériences réalisées.
